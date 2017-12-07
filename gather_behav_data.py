7789564192e97f5b78a160048578bd38f8c84e46
jelman <jelman523@gmail.com>
2017-12-07 12:38:33 -0800

    Exclude previously merged file from current list of files

--- gather_behav_data.py
+++ gather_behav_data.py
@@ -13,19 +13,22 @@ import pandas as pd
 from glob import glob
 
 datadir = '/home/jelman/netshare/VETSA_NAS/PROJ/LCIP/data/behavioral/raw/oddball'
+# Find all behavioral files
 globstr = os.path.join(datadir, 'OddballP300_LCI_Pilot_*.csv')
 filelist = glob(globstr)
+# Remove previously concatenated files from list
+filelist = [ f for f in filelist if "AllSubjects" not in f ]
 
+# Append subject files to master dataframe
 allsubs = pd.DataFrame()
-
-for fname in filelist:   
+for fname in filelist:
     subdat = pd.read_csv(fname)
     allsubs = allsubs.append(subdat)
-
+# Zero pad subject IDs
 allsubs['Subject_ID'] = allsubs['Subject_ID'].astype(str).apply(lambda x: x.zfill(3))
 allsubs['Subject_ID'] = 'LCIP' + allsubs['Subject_ID']
-
-allsubs = allsubs.sort(columns=['Subject_ID', 'Session'])
-
-outfile = os.path.join(datadir, 'OddballP300_LCI_Pilot_AllSubjects.csv')
+# Sort data
+allsubs = allsubs.sort_values(by=['Subject_ID', 'Session'])
+# Save out
+outfile = os.path.join(datadir, 'OddballP300_LCI_Pilot_AllSubjects_12062017.csv')
 allsubs.to_csv(outfile, index=False)
